48103be501626c5bfe88f438c86765965e73a54f
rakesh1533 <38495890+rakesh1533@users.noreply.github.com>
2020-07-28 20:07:27 +0530

    corona effect

--- src/main/java/com/rns/training/App.java
+++ src/main/java/com/rns/training/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcome to Digiinfotek" );
+        System.out.println( "Hello People this 2020 entire corona year" );
     }
 }
